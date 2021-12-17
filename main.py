9614a811e66ff80845186ff36c65bc72dca76f34
amine ziad ounnoughene <86957029+amine-ziad-ounnoughene@users.noreply.github.com>
2021-12-17 20:29:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,4 @@ time = st.time_input('when exactly did you lost it')
 st.subheader("description")
 txt = st.text_area('describe the losted object and where did you losted it ??', '''I losted a black and white Gucci glasses in the hamri street near the karan store mourad''')
 des = f"losted item : {lost} \n date : {date}  {time} \n {txt}"
+st.write(des)
