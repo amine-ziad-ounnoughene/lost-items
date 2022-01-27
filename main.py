7bdadcd41cbef4adb294aa9e2ce8117f2536ff14
amine ziad ounnoughene <86957029+amine-ziad-ounnoughene@users.noreply.github.com>
2022-01-27 18:00:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,57 @@
-import streamlit as st
-st.title("Losted items in Oran")
-st.header("Losted item")
-lost = st.text_input("what did you lost ??","glasses")
-st.header("contact")
-lo = st.text_input("how can we contact you??","+000000000")
-st.header("Date")
-date = st.date_input(
-     "When did you lost it ??")
-st.header("Time")
-time = st.time_input('when exactly did you lost it')
-st.header("description")
-txt = st.text_area('describe the losted object and where did you losted it ??', '''I losted a black and white Gucci glasses in the hamri street near the karan store mourad''')
-st.title("losted items")
-st.subheader(lost)
-st.subheader(f"date : {date}  {time}")
-st.subheader(f"contact : {lo}")
-st.subheader(txt)
+import sqlite3
+import streamlit as st 
+conn = sqlite3.connect("item.db",check_same_thread=False)
+cur = conn.cursor()
+
+def submit(item,num,time,date,mail,txt):
+    cur.execute("INSERT INTO items VALUES (?,?,?,?,?,?)" , (item,num,time,date,mail,txt))
+    conn.commit()
+def itemt(item,o):
+    with st.form(str(o)):
+        st.header("Losted item : " + item[0])
+        st.write("Phone number : " + item[1])
+        st.write("e-mail : " + item[4])
+        st.write("Date : " + item[3])
+        st.write("Time : " + item[2])
+        st.write("Item and place description : " + item[-1])
+        submitted = st.form_submit_button("")
+def form():
+    st.title("lost items")
+    with st.form("my form"):
+        item = st.text_input("Item","glasses")
+        num = st.text_input("Phone number","+000000000")
+        time = str(st.time_input('Time'))
+        date = str(st.date_input("Date"))
+        mail = st.text_input("e-mail","thestudent@gmail.com")
+        txt = st.text_area('Item and place Description', '''I losted a black and white Gucci glasses in the hamri street near the karan store mourad''')
+        button = st.form_submit_button(label="submit")
+        if button :
+            try : 
+                cur.execute('''CREATE TABLE IF NOT EXISTS items(
+                    item TEXT,
+                    num TEXT,
+                    time TEXT,
+                    date TEXT,
+                    mail TEXT,
+                    txt TEXT,
+                    PRIMARY KEY (txt , item,num,time,date,mail)
+                )''')
+                conn.commit()
+                submit(item,num,time,date,mail,txt)
+                st.success("you will find your item soon")
+            except:
+                st.error("existed item")
+        else :
+            pass
+    c = cur.execute("SELECT * FROM items")
+    conn.commit()
+    r = c.fetchall()
+    conn.close()
+    #st.write(c.fetchall())
+    if r != None:
+        a = 0
+        for o in r:
+            itemt(o,a)
+            a += 1
+
+form()
